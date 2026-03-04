5bfca0b58622377e27b46b681e66ea7f0cce9622
Karla <159499352+kar2inha@users.noreply.github.com>
2026-03-04 11:04:21 -0300

    feat: adicionando mais coisas para aparecer o gráfico

--- aula.py
+++ aula.py
@@ -24,3 +24,24 @@ angle_deg = np.degrees(angle_rad)
 
 # Criar gráfico
 plt.figure(figsize=(8, 6))
+
+# Tragetória
+plt.plot([A[0], B[0], C[0]], [A[1], B[1], C[1]], "bo-", label="Trajetória")
+
+# Vetores com setas
+plt.quiver(A[0], A[1], AB[0], AB[1], angles="xy", scale_units="xy", scale=1, color="r", label="Vetor AB")
+plt.quiver(B[0], B[1], BC[0], BC[1], angles="xy", scale_units="xy", scale=1, color="r", label="Vetor AB")
+
+# Anotar  pontos
+plt.text(A[0], A[1], "A", fontsize=12, ha="right")
+plt.text(B[0], B[1], "B", fontsize=12, ha="right")
+plt.text(C[0], C[1], "C", fontsize=12, ha="right")
+
+# Configurações do gráfico 
+plt.title(f"Deslocamento do Drone\nDist AB: {dist_AB:.2f}, Dist BC:{dist_BC:.2f}, Ângulo: {angle_deg:.2f}")
+plt.xlabel("X")
+plt.ylabel("Y")
+plt.grid(True)
+plt.axis("equal")
+plt.legend()
+plt.show()
